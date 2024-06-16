cf498658a5f782f41b91caf16a59e54c7392e165
Jan Kallwies <jan@kallwies.com>
2024-06-16 23:51:21 +0200

    Optimize the label colors

--- src/label_colors.cpp
+++ src/label_colors.cpp
@@ -8,13 +8,13 @@ QColor LabelColors::colorForLabelId(const int label_id)
     return QColor(0.894 * 255, 0.102 * 255, 0.110 * 255);
 
   case 1:
-    return QColor(0.216 * 255, 0.494 * 255, 0.722 * 255);
+    return QColor(0.596 * 255, 0.306 * 255, 0.639 * 255);
 
   case 2:
     return QColor(0.302 * 255, 0.686 * 255, 0.290 * 255);
 
   case 3:
-    return QColor(0.596 * 255, 0.306 * 255, 0.639 * 255);
+    return QColor(0.40 * 255, 0.761 * 255, 0.647 * 255);
 
   case 4:
     return QColor(1.00 * 255, 0.498 * 255, 0.0 * 255);
@@ -26,7 +26,7 @@ QColor LabelColors::colorForLabelId(const int label_id)
     return QColor(0.969 * 255, 0.506 * 255, 0.749 * 255);
 
   case 7:
-    return QColor(0.40 * 255, 0.761 * 255, 0.647 * 255);
+    return QColor(0.216 * 255, 0.494 * 255, 0.722 * 255);
 
   case 8:
   default:
